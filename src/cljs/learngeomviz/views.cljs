6d60449c4b7ecab64a5a40572f23f56910d84f71
ThinhPTran <trphthinh@gmail.com>
2016-12-19 10:02:35 +0700

    Add some more pictures

--- src/cljs/learngeomviz/views.cljs
+++ src/cljs/learngeomviz/views.cljs
@@ -34,8 +34,8 @@
   [spec]
   (->> spec
        (viz/svg-plot2d-cartesian)
-       (svg/svg {:width 600 :height 600})
-       ))
+       (svg/svg {:width 600 :height 600})))
+
 
 (def loglogspec
   {:x-axis (viz/log-axis
@@ -63,8 +63,8 @@
   [spec]
   (->> spec
        (viz/svg-plot2d-cartesian)
-       (svg/svg {:width 600 :height 600})
-       ))
+       (svg/svg {:width 600 :height 600})))
+
 
 (defn test-equation
   [t] (let [x (m/mix (- PI) PI t)] [x (* (Math/cos (* 0.5 x)) (Math/sin (* x x x)))]))
@@ -94,8 +94,8 @@
   [spec]
   (->> spec
        (viz/svg-plot2d-cartesian)
-       (svg/svg {:width 600 :height 320}))
-  )
+       (svg/svg {:width 600 :height 320})))
+
 
 (def linearareaspec
   {:x-axis (viz/linear-axis
@@ -122,14 +122,130 @@
   [spec]
   (->> spec
        (viz/svg-plot2d-cartesian)
-       (svg/svg {:width 600 :height 320}))
-  )
+       (svg/svg {:width 600 :height 320})))
+
+
+(def linepolarspec
+  {:x-axis (viz/linear-axis
+             {:domain [(- PI) PI]
+              :range  [(* 1.1 PI) (* 1.9 PI)]
+              :major  (/ PI 2)
+              :minor  (/ PI 16)
+              :pos    280})
+   :y-axis (viz/linear-axis
+             {:domain [-1 1]
+              :range  [60 280]
+              :major  0.5
+              :minor  0.25
+              :pos    (* 1.1 PI)})
+   :origin (v/vec2 300 310)
+   :grid   {:attribs {:stroke "#caa" :fill "none"}
+            :minor-x true
+            :minor-y true}
+   :data   [{:values  (map test-equation (m/norm-range 200))
+             :attribs {:fill "none" :stroke "#0af"}
+             :layout  viz/svg-line-plot}]})
+
+(defn linepolarimage
+  [spec]
+  (->> spec
+       (viz/svg-plot2d-polar)
+       (svg/svg {:width 600 :height 320})))
+
+(def areapolarspec
+  {:x-axis (viz/linear-axis
+             {:domain [(- PI) PI]
+              :range  [(* 1.1 PI) (* 1.9 PI)]
+              :major  (/ PI 2)
+              :minor  (/ PI 16)
+              :pos    280})
+   :y-axis (viz/linear-axis
+             {:domain [-1 1]
+              :range  [60 280]
+              :major  0.5
+              :minor  0.25
+              :pos    (* 1.1 PI)})
+   :origin (v/vec2 300 310)
+   :grid   {:attribs {:stroke "#caa" :fill "none"}
+            :minor-x true
+            :minor-y true}
+   :data   [{:values  (map test-equation (m/norm-range 200))
+             :attribs {:fill "#0af"}
+             :layout  viz/svg-area-plot}]})
+
+(defn areapolarimage
+  [spec]
+  (->> spec
+       (viz/svg-plot2d-polar)
+       (svg/svg {:width 600 :height 320})))
+
+(defn bar-spec
+  [num width]
+  (fn [idx col]
+    {:values     (map (fn [i] [i (m/random 100)]) (range 2000 2016))
+     :attribs    {:stroke       col
+                  :stroke-width (str (dec width) "px")}
+     :layout     viz/svg-bar-plot
+     :interleave num
+     :bar-width  width
+     :offset     idx}))
+
+(def bar-single-spec
+  {:x-axis (viz/linear-axis
+             {:domain [1999 2016]
+              :range  [50 580]
+              :major  1
+              :pos    280})
+   :y-axis (viz/linear-axis
+             {:domain      [0 100]
+              :range       [280 20]
+              :major       10
+              :minor       5
+              :pos         50
+              :label-dist  15
+              :label-style {:text-anchor "end"}})
+   :grid   {:minor-y true}
+   :data [((bar-spec 1 20) 0 "#0af")]})
+
+(defn bar-single-image
+  [spec]
+  (->> spec
+      (viz/svg-plot2d-cartesian)
+      (svg/svg {:width 600 :height 320})))
+
+(def bar-multi-spec
+  {:x-axis (viz/linear-axis
+             {:domain [1999 2016]
+              :range  [50 580]
+              :major  1
+              :pos    280})
+   :y-axis (viz/linear-axis
+             {:domain      [0 100]
+              :range       [280 20]
+              :major       10
+              :minor       5
+              :pos         50
+              :label-dist  15
+              :label-style {:text-anchor "end"}})
+   :grid   {:minor-y true}
+   :data (map-indexed (bar-spec 3 6) ["#0af" "#fa0" "#f0a"])})
+
+(defn bar-multi-image
+  [spec]
+  (->> spec
+       (viz/svg-plot2d-cartesian)
+       (svg/svg {:width 600 :height 320})))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div "Hello  " @name
+      [:div
        [loglinearimage loglinearspec]
        [loglogimage loglogspec]
        [linearimage linearspec]
-       [linearareaimage linearareaspec]])))
+       [linearareaimage linearareaspec]
+       [linepolarimage linepolarspec]
+       [areapolarimage areapolarspec]
+       [bar-single-image bar-single-spec]
+       [bar-multi-image bar-multi-spec]])))
+
